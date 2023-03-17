0f7a3c862c03335f4f2f03802b9de9d4b4d2115e
Luba-web <ldvasileva87@yandex.ru>
2023-03-17 22:22:16 +0300

    feat: add ecosystem

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Деплой приложения на сервер с использованием pm2
 
 Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+
+#### IP адрес: 51.250.101.243
+
+#### Frontend: https://mesto.luba-web.nomoredomains.work/
+
+#### Backend: https://api.mesto.luba-web.nomoredomains.work/
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'api-frontend',
+      script: '.src/index.js',
+    },
+  ],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};
